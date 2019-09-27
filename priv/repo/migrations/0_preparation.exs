b2b50ef5387a0aa3ace7468a07897791511a5da9
Tomas Sivicki <tomas.sivicki@gmail.com>
2019-09-27 11:46:21 +0200

    fix: wrote in migrations form

--- priv/repo/migrations/0_preparation.exs
+++ priv/repo/migrations/0_preparation.exs
@@ -1,3 +1,7 @@
-execute "CREATE EXTENSION pg_trgm;"
+defmodule Accent.Repo.Migrations.CreateExtensions do
+  use Ecto.Migration
 
-
+  def extensions do
+    execute "CREATE EXTENSION pg_trgm;"
+  end
+end
